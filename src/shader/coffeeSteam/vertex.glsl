eb9ea5fd0c1360355d905903568bbe3b96ddca67
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-11-04 13:35:03 -0400

    subtle detail on coffee steam added

--- src/shader/coffeeSteam/vertex.glsl
+++ src/shader/coffeeSteam/vertex.glsl
@@ -1,7 +1,20 @@
+uniform float uTime;
 varying vec2 vUv;
 
+#pragma glslify: perlin2d = require('../partials/perlin2d.glsl')
+
 void main() {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+    vec2 displacementUv = uv;
+    displacementUv *= 5.0;
+    displacementUv.y -= uTime * 0.001;
+
+    float displcementStrength = pow(uv.x * 2.5, 2.0);
+    float perlin = perlin2d(displacementUv) * displcementStrength;
+
+    newPosition.y += perlin * 0.5;
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
